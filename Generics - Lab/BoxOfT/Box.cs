0b9f7d57c78a5660814a716325c7213b8fac8f3b
ShreddingThrasher <100221813+ShreddingThrasher@users.noreply.github.com>
2022-05-05 13:20:50 +0300

    Initial commit

---
+++ Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> items;
+        public Box()
+        {
+            this.items = new Stack<T>();
+        }
+        public int Count { get => this.items.Count; }
+        public void Add(T element)
+        {
+            this.items.Push(element);
+        }
+        public T Remove()
+        {
+            return this.items.Pop();
+        }
+    }
+}
